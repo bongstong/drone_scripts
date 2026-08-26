776d917efe9d45010205546a34efe8e1842772cb
bongstong <nathan.vidal2197@gmail.com>
2026-08-26 13:56:03 +0200

    if camera is the main module then it starts filming

--- camera.py
+++ camera.py
@@ -6,11 +6,11 @@ import signal
 def start_recording() -> subprocess.Popen:
     """--- START RECORDING ---
     launch FFmpeg as a background process"""
-    CMD: str = "ffmpeg -f v4l2 -input_format mjpeg -video_size 1280x720 -i /dev/video0 -c:v libx264 -preset ultrafast -y flight.mp4"
-    print("Recording started in background...")
-    return subprocess.Popen(
-        CMD, shell=True, preexec_fn=os.setsid
+    CMD: str = (
+        "ffmpeg -f v4l2 -input_format mjpeg -video_size 1280x720 -i /dev/video0 -c:v libx264 -preset ultrafast -y flight.mp4"
     )
+    print("Recording started in background...")
+    return subprocess.Popen(CMD, shell=True, preexec_fn=os.setsid)
 
 
 def stop_recording(video_process: subprocess.Popen) -> None:
@@ -19,3 +19,9 @@ def stop_recording(video_process: subprocess.Popen) -> None:
     os.killpg(os.getpgid(video_process.pid), signal.SIGINT)
     print("Recording stopped and saved.")
     return None
+
+
+if __name__ == "__main__":
+    video_proces: subprocess.Popen = start_recording()
+    if input() == "q":
+        stop_recording(video_proces)
