031f13964e5a645f1597bf23ec786673a1d78619
bongstong <nathan.vidal2197@gmail.com>
2026-08-26 14:43:21 +0200

    fixed what had to be fixed before

--- camera.py
+++ camera.py
@@ -1,6 +1,4 @@
 import subprocess
-import os
-import signal
 
 
 def start_recording() -> subprocess.Popen:
@@ -12,15 +10,18 @@ def start_recording() -> subprocess.Popen:
         'ffmpeg -f v4l2 -input_format mjpeg -video_size 1280x720 -i /dev/video0 -c:v libx264 -preset ultrafast -vf "vflip,hflip" -y flight.mp4'
     )
     print("Recording started in background...")
-    return subprocess.Popen(CMD, shell=True, preexec_fn=os.setsid)
+    return subprocess.Popen(
+        CMD,
+        shell=True,
+        stdin=subprocess.PIPE,
+        start_new_session=True,
+    )
 
 
 def stop_recording(video_process: subprocess.Popen) -> None:
     """--- STOP RECORDING ---
     sends a 'Ctrl+C' signal to the background process to save the file"""
-    os.killpg(os.getpgid(video_process.pid), signal.SIGINT)
-    subprocess.run(["killall", "ffmpeg"])
-    print("Recording stopped and saved.")
+    video_process.communicate(input=b'q', timeout=10)
     return None
 
 
